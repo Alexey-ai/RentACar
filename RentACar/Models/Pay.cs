ad2c24cd0ed8abdd61d7192e73e613c48e199eb6
keosavl <vampirator@bk.ru>
2021-02-03 20:10:37 +0300

    add pay xml download

---
+++ RentACar/Models/Pay.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RentACar.Models
+{
+    [Serializable]
+    public class Pay
+    {
+        public DateTime Date { get; set; }
+        public string Name { get; set; }
+        public string Model { get; set; }
+        public int Price { get; set; }
+        public Pay()
+        {
+
+        }
+    }
+}
